e5c381e0dd284a6d0c18c7729dd779e155c7f787
Juan Camilo Gomez Lopez <31134291+juankg214@users.noreply.github.com>
2024-03-07 15:26:18 -0500

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -19,13 +19,11 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		driver.get("https://azurewebappjava3.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("Test PTC"));
 		driver.close();
-	
-		
 		
 	}
 }
